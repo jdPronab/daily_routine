0a36410c8c6a586ee77f481befbb044f64883a82
jdPronab <jdpronab@gmail.com>
2023-01-11 15:44:04 +0530

    screen manager implemented

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import kivy
+kivy.require('1.9.0')
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
+Builder.load_file("./all_alarm_screen.kv")
+Builder.load_file("./new_alarm_screen.kv")
+Builder.load_file("./menu.kv")
+
+class AllAlarmScreen(Screen):
+    pass
+
+class NewAlarmScreen(Screen):
+    pass
+
+class AlarmScreen(Screen):
+    pass
 
 class MainApp(App):
-    def build(self):
-        return Label(text="Daily Routine")
 
-if __name__ == "__main__":
-    main_app = MainApp()
-    main_app.run()
+    def build(self):
+        # Create the screen manager
+        sm = ScreenManager(transition=NoTransition())
+        sm.add_widget(AlarmScreen(name='alarm_screen'))
+        sm.add_widget(NewAlarmScreen(name='new_alarm_screen'))
+        sm.add_widget(AllAlarmScreen(name='all_alarm_screen'))
+        sm.current = 'alarm_screen'
+        return sm
+
+if __name__ == '__main__':
+    MainApp().run()
